87937b26193508b3a24bd3ceab0672bfbfb48c4a
Drishti <drishtirwt019@gmail.com>
2025-02-18 23:04:26 +0530

    hero adn features section

---
+++ components/Pricing.jsx
@@ -0,0 +1,19 @@
+'use client'
+import React from 'react'
+import { Element } from 'react-scroll'
+
+const Pricing = () => {
+  return (
+    <section>
+        <Element name="pricing">
+            <div className='container'>
+
+            </div>
+
+            </Element>
+      
+    </section>
+  )
+}
+
+export default Pricing
