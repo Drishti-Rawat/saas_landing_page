d57d07f08b7201b77acc6e8a4e28d48d1558bc59
Drishti <drishtirwt019@gmail.com>
2025-02-19 12:59:56 +0530

    pricing section

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import Features from '@/components/Features'
+import Pricing from '@/components/Pricing'
 
 const Homepage = () => {
   return (
@@ -9,6 +10,7 @@ const Homepage = () => {
       <Header />
       <Hero />
       <Features/>
+      <Pricing/>
 
     </main>
   )
