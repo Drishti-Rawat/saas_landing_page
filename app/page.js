11e9981b5931a048081cd2a21a94173b1dc02f7d
Drishti <drishtirwt019@gmail.com>
2025-02-19 20:20:21 +0530

    footer

--- app/page.js
+++ app/page.js
@@ -4,6 +4,9 @@ import Hero from '@/components/Hero'
 import Features from '@/components/Features'
 import Pricing from '@/components/Pricing'
 import Faq from '@/components/Faq'
+import Testimonials from '@/components/Testimonials'
+import Download from '@/components/Download'
+import Footer from '@/components/Footer'
 
 const Homepage = () => {
   return (
@@ -13,7 +16,9 @@ const Homepage = () => {
       <Features/>
       <Pricing/>
       <Faq/>
-
+      <Testimonials/>
+      <Download/>
+      <Footer/>
     </main>
   )
 }
